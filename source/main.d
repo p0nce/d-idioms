c5e2d146fbc1eec4e9b93c4269a66914b28fc9d2
ponce <aliloko@gmail.com>
2015-04-30 01:32:16 +0200

    Add link and more spaces in CSS.

--- source/main.d
+++ source/main.d
@@ -90,6 +90,18 @@ void main(string[] args)
                         }
                     pop;
                     writeln("<a href=\"https://github.com/p0nce/d-idioms/\"><img style=\"position: absolute; top: 0; right: 0; border: 0;\" src=\"https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67\" alt=\"Fork me on GitHub\" data-canonical-src=\"https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png\"></a>");
+
+
+                     push("footer");
+
+                        push("h2");
+                            writeln("About");
+                        pop;
+
+                        push("p");
+                            writeln(`Hi, I'm a <a href="http://github.com/p0nce">software developer</a> currently building <a href="http://www.auburnsounds.com">real-time audio plugins for voice</a>. I hope this site has been useful!`);
+                        pop();
+                    pop;
                 pop;
             pop;
         }
